e298c2dbfffbd4d0594d793eb30d09029dd04fcc
dovanhung218 <bills2k2@gmail.com>
2022-05-15 22:17:14 +0700

    first commit

---
+++ moralis-react-authentication/src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MoralisProvider } from "react-moralis";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "light",
+  },
+});
+
+const moralisAppId = "rcK4ZY6L3aBDh5VwTKMytBgXPCflGCfpmIoXdLDj";
+const moralisServerURL = "https://mujyxjshyywb.usemoralis.com:2053/server";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId={moralisAppId} serverUrl={moralisServerURL}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
